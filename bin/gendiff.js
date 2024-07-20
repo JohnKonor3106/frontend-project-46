f2ab6f65786215f120e998e43cb8d617475ba787
John <alexway124@gmail.com>
2024-07-20 03:22:10 +0300

    create project

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+import { Command } from 'commander';
+const program = new Command();
+
+    program
+      .name('string-util')
+      .description('CLI to some JavaScript string utilities')
+      .version('0.8.0');
+    
+    program.parse();
+
+
+
+
+
+
