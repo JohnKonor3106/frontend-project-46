1e39f203fdb3f4590359235304a20621709b625b
John <alexway124@gmail.com>
2024-07-21 13:46:45 +0300

    added the ability to justify arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,14 @@
 import { Command } from 'commander';
+
 const program = new Command();
 
-    program
-      .name('string-util')
-      .description('CLI to some JavaScript string utilities')
-      .version('0.8.0');
-    
-    program.parse();
+program
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0')
+  .option('-f, --format', 'output format')
+  .argument('<file1path1>')
+  .argument('<file1path2>')
+  
 
-
-
-
-
-
+program.parse();
